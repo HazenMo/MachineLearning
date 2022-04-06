056be3da34c0cfd82ad8f11ddaed53e85db3d7a5
Mosiah Hazen <haz17005@byui.edu>
2022-04-05 22:42:54 -0600

    read me

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# MachineLearning
+# Overview
+
+I am using Machine Learning to interpret a data set of hand written digits (numbers between 0 and 9). The data set is provided by MNST but at the end of the program there is an opportunity to introduce your own "hand written" digit for the model to interpret. 
+
+I created this program not so much to interpret a data set, but mostly to learn about neural networks and machine vision. I wanted to explore the creation and training of a nueral network, and then to implement that training to do some deductions. 
+
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Data Analysis Results
+
+The neural network usually finishes it's training with an accuracy of about 97% to 98%. Though with experimentation usually it does not do as well when the completely new instance is interpreted. I believe this is a by product of overfitting. 
+
+# Development Environment
+
+I used VS Code as the IDE for this project.
+
+I wrote this project in Python and used a number of libraries to make this project work. Fist and foremost I used tensorflow to build my neural network. I also used numpy and matplotlib to create the displays that show the data set and labels so you get an idea what is being fed into the network. Finally I used pygame and PIL so the user can create their own piece of data to test the network. Pygame creates the drawn on image, and PIL allows the program to format the image into something the network can actually interpret.
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [TensorFlow](https://www.tensorflow.org/tutorials)
+* [GeeksforGeeks](https://www.geeksforgeeks.org/)
+
+# Future Work
+
+* I definitly want to fix the overfitting problem that the program has. I have a few ideas of how I might do this but the top of my list is currently to use data generation tools to modify the data set, so that it might get used to numbers that are shown a little differently.
+* I also want to figure a way to save the trained network so that it has the ability to be used without training it, every singe time.
+* Finally I think it would be awesome to expand the network beyond just digits to handwritten letters as well, this would be as simple as adding more data to the training, but I would eventually want to put it to work interpreting handwritten words and phrases from say a phone camera.
--- neural_network.py
+++ neural_network.py
@@ -1,3 +1,4 @@
+# Import all needed libraries.
 from turtle import color
 import tensorflow as tf
 import numpy as np
@@ -6,31 +7,34 @@ import pygame
 from PIL import Image
 from PIL import Image, ImageOps
 
-
+# Declare dataset.
 mnist = tf.keras.datasets.mnist
 
+# Pull relevant data from the set.
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data(path = 'mnist.npz')
 
 train_images = train_images / 255
 test_images = test_images / 255
 
+# Display dataset, to understand what the computer sees.
+plt.figure(figsize=(8, 8))
+for i in range (16):
+    plt.subplot(4, 4, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(train_labels[i])
+plt.show()
 
-# plt.figure(figsize=(8, 8))
-# for i in range (16):
-#     plt.subplot(4, 4, i + 1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.grid(False)
-#     plt.imshow(train_images[i], cmap=plt.cm.binary)
-#     plt.xlabel(train_labels[i])
-# plt.show()
-
+# Declare the "shape" of the model.
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(10)
 ])
 
+# Run the model.
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
@@ -39,6 +43,7 @@ model.fit(train_images, train_labels, epochs=7)
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 print('\nTest accuracy:', test_acc)
 
+# Save the model to variable.
 probability_model = tf.keras.Sequential([model,
                                          tf.keras.layers.Softmax()])
 
@@ -48,6 +53,7 @@ predictions[0]
 np.argmax(predictions[0])
 test_labels[0]
 
+# Display model results to user.
 def plot_image(i, predictions_array, true_label, img):
     true_label, img = true_label[i], img[i]
     plt.grid(False)
@@ -79,18 +85,19 @@ def plot_value_array(i, predictions_array, true_label):
     thisplot[predicted_label].set_color('red')
     thisplot[true_label].set_color('blue')
 
-# num_rows = 3
-# num_cols = 3
-# num_images = num_rows * num_cols
-# plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
-# for i in range(num_images):
-#     plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
-#     plot_image(i, predictions[i], test_labels, test_images)
-#     plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
-#     plot_value_array(i, predictions[i], test_labels)
-# plt.tight_layout()
-# plt.show()
+num_rows = 3
+num_cols = 3
+num_images = num_rows * num_cols
+plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+    plot_image(i, predictions[i], test_labels, test_images)
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+    plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
 
+# Create a pygame window where the user can draw a new digit.
 pygame.init()
 white = [255,255,255]
 black = [0,0,0]
@@ -98,14 +105,14 @@ size = [400,400]
 poly_list = []
 screen=pygame.display.set_mode(size)
 pygame.display.set_caption("Test")
-pygame.mouse.set_visible(0)
+pygame.mouse.set_visible(100)
 done = False
 mouse_down = False
 clock = pygame.time.Clock()
 
 def draw_cursor(screen,x,y):
     if mouse_down:
-        polygon = ([x,y], [x-17, y], [x-17,y-17], [x,y-17])
+        polygon = ([x,y], [x-22, y], [x-22,y-22], [x,y-22])
         poly_list.append(polygon)
 
 while done==False:
@@ -133,6 +140,7 @@ pygame.image.save(screen, 'number.jpg')
 
 pygame.quit()
 
+# Save image from pygame and refigure to meet models needs.
 image = Image.open('number.jpg').convert('L')
 resized = image.resize((28, 28))
 resized = ImageOps.invert(resized)
@@ -140,11 +148,16 @@ resized = ImageOps.invert(resized)
 
 img = (np.expand_dims(resized, 0))
 
+# Test digit against model.
 predictions_single = probability_model(img)
 
+# Get correct label from user.
+test_input = int(input('What number did you draw? '))
+
+# Display models predictions against correct label.
 plt.figure(figsize=(6,3))
 plt.subplot(1,2,1)
-plot_image(0, predictions_single[0], test_labels, img)
+plot_image(0, predictions_single[0], (test_input,), img)
 plt.subplot(1,2,2)
-plot_value_array(0, predictions_single[0],  test_labels)
+plot_value_array(0, predictions_single[0],  (test_input,))
 plt.show()
